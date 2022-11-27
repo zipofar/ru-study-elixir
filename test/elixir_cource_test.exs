1d640d803bc5b21495811f5facb3f006331272f2
Nikita <nikita.davydov@dualbootpartners.com>
2022-11-27 17:09:29 +0400

    [#13] Введение в программирование Elixir. List

--- test/elixir_cource_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ElixirCourceTest do
-  use ExUnit.Case
-  doctest ElixirCource
-
-  test "greets the world" do
-    assert ElixirCource.hello() == :world
-  end
-end
---
+++ test/exercises/list_test.exs
@@ -0,0 +1,34 @@
+defmodule ElixirCource.ListExerciseTest do
+  use ExUnit.Case
+  alias ElixirCource.ListExercise
+
+  @moduletag :skip
+
+  describe "List Exercise" do
+    test "replace" do
+      input_list = [3, 2, -8, 4, 100, -6, 7, 8, -99]
+      replaced_list = ListExercise.replace(input_list)
+      assert replaced_list == [100, 100, -8, 100, 100, -6, 100, 100, -99]
+
+      input_list = [-1, -2, -3, -4]
+      replaced_list = ListExercise.replace(input_list)
+      assert replaced_list == [-1, -2, -3, -4]
+
+      input_list = []
+      replaced_list = ListExercise.replace(input_list)
+      assert replaced_list == []
+    end
+
+    test "search" do
+      assert ListExercise.search([1], 900) == -1
+      assert ListExercise.search([1], 1) == 0
+      assert ListExercise.search([], 900) == -1
+      assert ListExercise.search([1, 4, 5, 7, 8, 9], 9) == 5
+      assert ListExercise.search([1, 4, 5, 7, 8, 9], 1) == 0
+      assert ListExercise.search([1, 4, 5, 7, 8, 9], 6) == -1
+
+      input_list = Enum.map(0..1000, fn number -> number end)
+      assert ListExercise.search(input_list, Enum.at(input_list, 999)) == 999
+    end
+  end
+end
